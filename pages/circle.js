3ffe86b9ef203b333a74ec03fcab57f6f3353428
devdutt-pedalsup <devdutt@pedalsup.com>
2023-01-09 18:37:58 +0530

    some more animations

---
+++ pages/circle.js
@@ -0,0 +1,19 @@
+import styles from "../styles/Circle.module.css";
+
+export default ( ) => {
+  let init = 0;
+  const touched = (e) => {
+    init += e.deltaY;
+    const ele = document.querySelector(".child");
+    console.log(ele.style.transform);
+    let value = (init*20)/66.25;
+    ele.style["rotate"] = `${value}deg `;
+    // console.log(value);
+  }
+
+  return (<>
+    <div className={styles.crcl} onWheel={(y) => { touched(y) }}>
+      <div className="child">Text for child</div>
+    </div>
+  </>)
+}
---
+++ pages/open.js
@@ -0,0 +1,14 @@
+import styles from "../styles/Open.module.css";
+
+export default () => {
+  return (
+    <>
+    <div className={styles.container}>
+      <div className={styles.div1}>
+        <span className={styles.spanblock}>Title</span>
+        <span className={styles.description}>description</span>
+      </div>
+    </div>
+    </>
+  )
+}
